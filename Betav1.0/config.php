d7a4dbb5626e3903f982b4060f0c7787c0688bc0
Gabriel de Ioannes Becker <gdeioannes@gmail.com>
2018-06-05 19:01:51 -0300

    Site Beta

---
+++ Betav1.0/config.php
@@ -0,0 +1,6 @@
+<?php
+
+//SITE GLOBAL CONFIGURATION
+$email = "capacitacion@inclub.cl";   //<-- Your email
+
+?>
---
+++ Betav1.0/js/inclub.js
@@ -0,0 +1,5 @@
+
+function changeSubject(val){
+    document.getElementById("subject").value=val;
+    console.log(document.getElementById("subject").value);
+}
