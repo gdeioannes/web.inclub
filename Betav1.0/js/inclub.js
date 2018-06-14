50ad9ba806844cfe6651b67bd1fb871beee928d9
Gabriel de Ioannes Becker <gdeioannes@gmail.com>
2018-06-14 04:22:49 -0300

    Las Changes

--- Betav1.0/js/inclub.js
+++ Betav1.0/js/inclub.js
@@ -2,4 +2,10 @@
 function changeSubject(val){
     document.getElementById("subject").value=val;
     console.log(document.getElementById("subject").value);
+    
+    var mailText="mailto:capacitacion@inclub.cl?Subject=*";
+    mailText=mailText.replace("*",val)
+    document.getElementById("mail-ref").setAttribute('href',mailText);
+    
+    
 }
